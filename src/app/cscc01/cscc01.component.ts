bd74ca5a6fece4089eb7987314760621cf4c8230
Yong Cho Yin <choyiny@users.noreply.github.com>
2019-09-25 16:36:25 -0400

    neo4j basic installation

--- src/app/cscc01/cscc01.component.ts
+++ src/app/cscc01/cscc01.component.ts
@@ -12,6 +12,7 @@ export class Cscc01Component implements OnInit {
     {title: 'Week 2', subtitle: 'Using Git and Github', markdownURL: '/assets/notes/cscc01/git.md'},
     {title: 'Week 3', subtitle: 'User Stories', markdownURL: '/assets/notes/cscc01/agile.md'},
     {title: 'Week 4', subtitle: 'REST', markdownURL: '/assets/notes/cscc01/rest.md'},
+    {title: 'Week 5', subtitle: 'neo4j', markdownURL: '/assets/notes/cscc01/neo.md'},
   ];
 
   constructor(private markdownService: MarkdownService) { }
